8d0f24b08071a866d216795cdf7bf15b871fd3d7
Thomas Bille <thomas.bille@canonical.com>
2019-09-26 11:36:11 +0100

    Add persistence

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-from telegram.ext import Updater, CommandHandler
 import os
 import requests
+from telegram.ext import Updater, CommandHandler, PicklePersistence
 
 def totobot(bot, update):
     response = requests.get('http://smoke.toto.space', headers={"Content-Type": "application/json"})
@@ -9,9 +9,10 @@ def totobot(bot, update):
     update.message.reply_text(
         'toto is winning even after {} days'.format(json["days"]))
 
+my_persistence = PicklePersistence(filename='my_file')
 
 token = os.getenv('TOKEN')
-updater = Updater(token)
+updater = Updater(token, persistence=my_persistence, use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('toto', totobot))
 
